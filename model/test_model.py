b6534c397735e34e4f0356345ed15f685218615c
= <dano.dovhun@gmail.com>
2024-03-18 20:12:07 +0100

    Created this branch

--- model/test_model.py
+++ model/test_model.py
@@ -27,8 +27,8 @@ titles = [
     ["Diverse cabinet is no ‘get-out-of-jail-free card’ on racism, says ex-No 10 adviser", 0],
     ["‘Complex’ post-Brexit tax rules means price rises for UK wine drinkers", 0],
     ["Post-2019 UK cabinet ministers last average of eight months, study finds", 0],
-    ["Millions more in cash needed to fund UK’s open-banking watchdog", 2],
-    ["Victims of Norton Motorcycles pension fraud paid £9.4m compensation", 2]
+    ["Millions more in cash needed to fund UK’s open-banking watchdog", 0],
+    ["Victims of Norton Motorcycles pension fraud paid £9.4m compensation", 0]
 ]
 
 articles = [
