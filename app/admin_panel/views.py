7e21cced6b017ebdf1d3419d5b2338a6283d2fc0
= <dano.dovhun@gmail.com>
2024-03-22 15:01:35 +0100

    An administrator can retrain the model

--- app/admin_panel/views.py
+++ app/admin_panel/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render,redirect
-from .models import AdminUser
+from .models import AdminUser, ModelInfo
 from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth.decorators import login_required
+from model.model import train
+from sklearn.metrics import ConfusionMatrixDisplay  
+
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def admin_login(request):
     if request.method == 'POST':
@@ -34,8 +39,26 @@ def admin_dashboard(request):
 
 @login_required
 def admin_model(request):
-      print("Hello world")
-      return render(request, 'admin_model.html')
+    ai_model = ModelInfo.objects.all()
+    model_info = ai_model[0]
+
+    acc = []
+
+    if model_info.old_acc == -1:
+        acc.append("N/A")
+
+    else:
+        acc.append(round(model_info.old_acc, 5))
+
+    return render(request, 'admin_model.html', {
+        "conf_mat": model_info.conf_matrix,
+        "train_time": model_info.training_time,
+        "old_acc": acc[0],
+        "accuracy": model_info.accuracy,
+        "precision": model_info.precision,
+        "recall": model_info.recall,
+        "f_one": model_info.f_one
+    })
 
 @login_required
 def admin_reports(request):
@@ -61,5 +84,43 @@ def admin_admins(request):
     admin_users = AdminUser.objects.all()
     return render(request, 'admin_admins.html', {'admin_users': admin_users})
 
- 
+@login_required
+def retrain(user):
+    cf_loc = "admin_panel/static/conf_mat.png"
+    training_time, score, conf_mat, recall, prec, acc, f_one = train()
+    ai_model = ModelInfo.objects.all()
 
+    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(10, 5))
+    sns.heatmap(conf_mat, cmap='Blues',
+                fmt='', annot=True, cbar=False,
+                annot_kws={'fontsize': 10, 'fontweight': 'bold'},
+                square=False)
+
+    ax.set_title("Confusion Matrix")
+    plt.tight_layout()
+    plt.savefig(cf_loc)
+
+    if len(ai_model) == 0:
+        model = ModelInfo(
+            conf_matrix = cf_loc,
+            training_time = training_time,
+            old_acc = -1,
+            accuracy = acc,
+            precision = prec,
+            recall = recall,
+            f_one = f_one
+        )
+
+        model.save()
+
+    else:
+        ai_model[0].training_time = training_time
+        ai_model[0].old_acc = ai_model[0].accuracy
+        ai_model[0].accuracy = acc
+        ai_model[0].precision = prec
+        ai_model[0].recall = recall
+        ai_model[0].f_one = f_one
+
+        ai_model[0].save()
+
+    return redirect("/admin/model")
--- app/app/urls.py
+++ app/app/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('admin/model/', adminViews.admin_model, name='admin_model'),
     path('admin/reports/', adminViews.admin_reports, name='admin_reports'),
     path('admin/admins/', adminViews.admin_admins, name='admin_admins'),
-    path('logout/' , adminViews.admin_logout, name='admin_logout')
-    
+    path('logout/' , adminViews.admin_logout, name='admin_logout'),
+    path('retrain/', adminViews.retrain)
 ]
