2bab45b26bcd7aeec5cbe2f618f1926ade840935
Edit-3D <aadi.nair@gmail.com>
2024-03-22 17:24:16 +0100

    Create Model for User Reports in db
    Created the model for the User Reports that so that they can be stored in the Database.  The report includes: the user inputted paragraph, the models prediction, the users prediction, URL to the article (optional) , and comments (optional)

--- app/users_panel/models.py
+++ app/users_panel/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class UserReport(models.Model):
+    USER_PREDICTION_CATEGORIES = [
+        ("", "Choose a Category"),
+        ("cat_bus", "Business"),
+        ("cat_ent", "Entertainment"),
+        ("cat_pol", "Politics"),
+        ("cat_sport", "Sports"),
+        ("cat_tech", "Technology"),
+    ]
+
+    reportParagraph = models.TextField()
+    reportModelPrediction = models.CharField(max_length=20)
+    reportUserPrediction = models.CharField(
+        choices=USER_PREDICTION_CATEGORIES, max_length=20
+    )
+    reportURL = models.URLField(blank=True)
+    reportComment = models.TextField(blank=True)
+    reportTime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"User Report - {self.reportParagraph[:20]} ..."
