cb45969481b490d1a152b7dd7b47c5e1dd49fce7
Edit-3D <aadi.nair@gmail.com>
2024-03-22 22:54:04 +0100

    Create User Reports for user
    * Created  user reports form, which is based on the model and takes initial values from the classification. this is done in modal.
    * Split the user side website into two templates. one for inputting the text and the other for receiving the prediction+user reporting.
    * Redid the views so that theres one for the input page and another for the predictions+reporting.
    * User reports now store in DB

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,18 +1,16 @@
-
 from django.contrib import admin
 from django.urls import path
 from users_panel import views as userViews
 from admin_panel import views as adminViews
 
 urlpatterns = [
-    
-    path('', userViews.user_dashboard, name='user'),
-    path("login/", adminViews.admin_login, name='admin_login'),
+    path("", userViews.user_dashboard, name="user"),
+    path("prediction/", userViews.user_prediction, name="user_prediction"),
+    path("login/", adminViews.admin_login, name="admin_login"),
     path("admin/", adminViews.admin_dashboard, name="admin"),
+    path("admin/model/", adminViews.admin_model, name="admin_model"),
+    path("admin/reports/", adminViews.admin_reports, name="admin_reports"),
+    path("admin/admins/", adminViews.admin_admins, name="admin_admins"),
+    path("logout/", adminViews.admin_logout, name="admin_logout"),
     path("test/", admin.site.urls),
-    path('admin/model/', adminViews.admin_model, name='admin_model'),
-    path('admin/reports/', adminViews.admin_reports, name='admin_reports'),
-    path('admin/admins/', adminViews.admin_admins, name='admin_admins'),
-    path('logout/' , adminViews.admin_logout, name='admin_logout')
-    
 ]
--- app/users_panel/views.py
+++ app/users_panel/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from model.model import classify
 
-from .forms import ArticleForm
+from .forms import ArticleForm, ReportForm
+from .models import UserReport
+
 
 def user_dashboard(request):
     categories = {
@@ -9,31 +11,55 @@ def user_dashboard(request):
         1: "Sport",
         2: "Technology",
         3: "Entertainment",
-        4: "Business"
+        4: "Business",
     }
-    
-    if request.method == 'POST':
-        form = ArticleForm(request.POST)
 
-        if form.is_valid():
-            search_input = form.cleaned_data["paragraph"]
+    if request.method == "POST":
+        article_form = ArticleForm(request.POST)
+
+        if article_form.is_valid():
+            search_input = article_form.cleaned_data["paragraph"]
 
             cat = classify(search_input)
             category = categories[cat[0]]
 
             print(category)
 
-        else:
-            return redirect("/")
+            request.session["search_input"] = search_input
+            request.session["category"] = category
 
-        return render(request, 'user_dashboard.html', {
-            'search_input': search_input,
-            "category": category,
-        })
+            return redirect("user_prediction")
 
     else:
-        form = ArticleForm()
+        article_form = ArticleForm()
+        return render(request, "user_dashboard.html", {"form": article_form})
 
-        return render(request, 'user_dashboard.html', {
-            "form": form,
-        })
+
+def user_prediction(request):
+    if request.method == "POST":
+        report_form = ReportForm(request.POST)
+        if report_form.is_valid():
+            print(f"is valid")
+            report = report_form.save()
+            return redirect("user_prediction")
+        else:
+            return render(request, "user_prediction.html", {"report_form": report_form})
+
+    else:
+        search_input = request.session.get("search_input", "")
+        category = request.session.get("category", "")
+        report_form = ReportForm(
+            initial={
+                "reportParagraph": search_input,
+                "reportModelPrediction": category,
+            }
+        )
+        return render(
+            request,
+            "user_prediction.html",
+            {
+                "search_input": search_input,
+                "category": category,
+                "report_form": report_form,
+            },
+        )
