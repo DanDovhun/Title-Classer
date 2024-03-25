828f21ad6fd3063a123374a8dc79d86b63c2005d
= <dano.dovhun@gmail.com>
2024-03-25 21:54:46 +0100

    When admin visits /admin they're redirected to /admin/model

--- app/admin_panel/views.py
+++ app/admin_panel/views.py
@@ -36,7 +36,9 @@ def admin_logout(request):
 
 @login_required
 def admin_dashboard(request):
-      return render(request, 'admin_dashboard.html')
+    #return render(request, 'admin_dashboard.html')
+    print("Hello world")
+    return redirect("/admin/model")
 
 @login_required
 def admin_model(request):
